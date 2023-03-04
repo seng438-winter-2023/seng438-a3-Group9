89b453e4b78bce6cd2575879ce76ce95e92f3d85
Saman Pordanesh <sina.pordanesh@yahoo.com>
2023-03-03 17:15:11 -0700

    test update Data utilities

--- JFreeChart_Lab3/jfreechart-1.0.19/source/org/jfree/data/datautilities/test/DataUtilitiesTest.java
+++ JFreeChart_Lab3/jfreechart-1.0.19/source/org/jfree/data/datautilities/test/DataUtilitiesTest.java
@@ -23,7 +23,11 @@ public class DataUtilitiesTest extends DataUtilities {
 
     }
 
-    //
+    // -------------------------------------------------
+
+    /**
+     *
+     */
     @Test // old
     public void calculateColumnTotalTest() {
 
@@ -32,9 +36,7 @@ public class DataUtilitiesTest extends DataUtilities {
         mockingContext.checking(new Expectations() {
             {
                 one(values).getRowCount(); will(returnValue(2));
-
                 one(values).getColumnCount(); will(returnValue(2));
-
                 one(values).getValue(0, 0); will(returnValue(10));
                 one(values).getValue(1, 0); will(returnValue(20));
                 one(values).getValue(0, 1); will(returnValue(30));
@@ -63,8 +65,7 @@ public class DataUtilitiesTest extends DataUtilities {
         assertEquals(5.0, result, .000000001d);
     }
 
-    @Test
-
+    @Test // new
     public void calculateColumnTotalWithInvalidInput() {
 
         Mockery mockingContext = new Mockery();
@@ -80,6 +81,143 @@ public class DataUtilitiesTest extends DataUtilities {
         });
         double result = DataUtilities.calculateColumnTotal(values, -1);
         assertEquals(0.0, result, .000000001d);
+    }
+
+    @Test // new
+    public void testCalculateColumnTotalWithValidRowsSpecified() {
+
+        Mockery mockingContext = new Mockery();
+        final Values2D values = mockingContext.mock(Values2D.class);
+        mockingContext.checking(new Expectations() {
+            {
+                one(values).getRowCount();will(returnValue(6));
+                one(values).getValue(0, 0);will(returnValue(7.5));
+                one(values).getValue(1, 0);will(returnValue(2.5));
+                one(values).getValue(2, 0);will(returnValue(-3));
+                one(values).getValue(3, 0);will(returnValue(-2));
+                one(values).getValue(4, 0);will(returnValue(7));
+                one(values).getValue(5, 0);will(returnValue(-3.5));
+            }
+        });
+        int[] validrows = {0,1,2,4};
+        double result = DataUtilities.calculateColumnTotal(values, 0, validrows);
+        assertEquals(14.0, result, .000000001d);
+    }
+
+    @Test //new
+    public void testCalculateColumnTotalWithInvalidRows() {
+        Mockery mockingContext = new Mockery();
+        final Values2D values = mockingContext.mock(Values2D.class);
+        mockingContext.checking(new Expectations() {
+            {
+                one(values).getRowCount();will(returnValue(6));
+                one(values).getValue(0, 0);will(returnValue(7.5));
+                one(values).getValue(1, 0);will(returnValue(2.5));
+                one(values).getValue(2, 0);will(returnValue(-3));
+                one(values).getValue(3, 0);will(returnValue(-2));
+                one(values).getValue(4, 0);will(returnValue(7));
+                one(values).getValue(5, 0);will(returnValue(-3.5));
+            }
+        });
+        int[] validrows = {10,11,12,14};
+        double result = DataUtilities.calculateColumnTotal(values, 0, validrows);
+        assertEquals(0.0, result, .000000001d);
+    }
+
+    @Test // new
+    public void testCalculateColumnTotalWithValidRowsNullValues() {
+
+        Mockery mockingContext = new Mockery();
+        final Values2D values = mockingContext.mock(Values2D.class);
+        mockingContext.checking(new Expectations() {
+            {
+                one(values).getRowCount();will(returnValue(6));
+                one(values).getValue(0, 0);will(returnValue(null));
+                one(values).getValue(1, 0);will(returnValue(null));
+                one(values).getValue(2, 0);will(returnValue(null));
+                one(values).getValue(3, 0);will(returnValue(-2));
+                one(values).getValue(4, 0);will(returnValue(null));
+                one(values).getValue(5, 0);will(returnValue(-3.5));
+            }
+        });
+        int[] validrows = {0,1,2,4};
+        double result = DataUtilities.calculateColumnTotal(values, 0, validrows);
+        assertEquals(0.0, result, .000000001d);
+    }
+
+    @Test // new
+    public void testCalculateColumnTotalWithNullValues() {
+        Mockery mockingContext = new Mockery();
+        final Values2D values = mockingContext.mock(Values2D.class);
+        mockingContext.checking(new Expectations() {
+            {
+                one(values).getRowCount();will(returnValue(6));
+                one(values).getValue(0, 0);will(returnValue(7.5));
+                one(values).getValue(1, 0);will(returnValue(2.5));
+                one(values).getValue(2, 0);will(returnValue(-3));
+                one(values).getValue(3, 0);will(returnValue(-2));
+                one(values).getValue(4, 0);will(returnValue(null));
+                one(values).getValue(5, 0);will(returnValue(null));
+            }
+
+        });
+        double result = DataUtilities.calculateColumnTotal(values, 0);
+        assertEquals(5.0, result, .000000001d);
+    }
+
+
+    @Test // new
+    public void calculateColumnTotalForFourValues() {
+        Mockery mockingContext = new Mockery();
+        final Values2D values = mockingContext.mock(Values2D.class);
+        mockingContext.checking(new Expectations() {
+            {
+                one(values).getRowCount();
+                will(returnValue(4));
+                one(values).getValue(0, 0);
+                will(returnValue(7.5));
+                one(values).getValue(1, 0);
+                will(returnValue(2.5));
+                one(values).getValue(2, 0);
+                will(returnValue(-3));
+                one(values).getValue(3, 0);
+                will(returnValue(-2));
+            }
+        });
+        double result = DataUtilities.calculateColumnTotal(values, 0);
+        assertEquals(5.0, result, .000000001d);
+    }
+
+    @Test(expected = InvalidParameterException.class) // new
+    public void calculateColumnTotalWithInvalidParameter() throws InvalidParameterException {
+        Mockery mockingContext = new Mockery();
+        final Values2D values = mockingContext.mock(Values2D.class);
+        mockingContext.checking(new Expectations() {
+            {
+                one(values).getRowCount();
+                will(returnValue(null));
+                one(values).getColumnCount();
+                will(returnValue(null));
+            }
+        });
+        double result = DataUtilities.calculateColumnTotal(values, 0);
+        assertEquals(0, result, .000000001d);
+    }
+
+    @Test(expected = InvalidParameterException.class) // new
+    public void calculateRowTotalWithInvalidParameter() throws InvalidParameterException {
+        Mockery mockingContext = new Mockery();
+        final Values2D values = mockingContext.mock(Values2D.class);
+        mockingContext.checking(new Expectations() {
+            {
+                one(values).getRowCount();
+                will(returnValue(null));
+                one(values).getColumnCount();
+                will(returnValue(null));
+            }
+        });
+        double result = DataUtilities.calculateRowTotal(values, 0);
+        assertEquals(0, result, .000000001d);
     }
 
     //-----------------------------------
@@ -139,6 +277,111 @@ public class DataUtilitiesTest extends DataUtilities {
         assertEquals(5.0, result, .000000001d);
     }
 
+    @Test // new
+    public void calculateRowTotalWithNullValues() {
+        Mockery mockingContext = new Mockery();
+        final Values2D values = mockingContext.mock(Values2D.class);
+        mockingContext.checking(new Expectations() {
+            {
+                one(values).getColumnCount();
+                will(returnValue(4));
+                one(values).getValue(0, 0);
+                will(returnValue(7.5));
+                one(values).getValue(0, 1);
+                will(returnValue(2.5));
+                one(values).getValue(0, 2);
+                will(returnValue(null));
+                one(values).getValue(0, 3);
+                will(returnValue(null));
+            }
+        });
+        double result = DataUtilities.calculateRowTotal(values, 0);
+        assertEquals(10.0, result, .000000001d);
+    }
+
+    @Test // new
+    public void calculateRowTotalWithValidColumns() {
+        Mockery mockingContext = new Mockery();
+        final Values2D values = mockingContext.mock(Values2D.class);
+        mockingContext.checking(new Expectations() {
+            {
+                one(values).getColumnCount();
+                will(returnValue(6));
+                one(values).getValue(0, 0);
+                will(returnValue(7.5));
+                one(values).getValue(0, 1);
+                will(returnValue(2.5));
+                one(values).getValue(0, 2);
+                will(returnValue(-3));
+                one(values).getValue(0, 3);
+                will(returnValue(-2));
+                one(values).getValue(0, 4);
+                will(returnValue(5));
+                one(values).getValue(0, 5);
+                will(returnValue(6));
+            }
+        });
+        int[] validcolumns = {0,2,3,5};
+        double result = DataUtilities.calculateRowTotal(values, 0, validcolumns);
+        assertEquals(8.5, result, .000000001d);
+    }
+
+    @Test // new
+    public void calculateRowTotalWithValidColumnsNullValues() {
+        Mockery mockingContext = new Mockery();
+        final Values2D values = mockingContext.mock(Values2D.class);
+        mockingContext.checking(new Expectations() {
+            {
+                one(values).getColumnCount();
+                will(returnValue(6));
+                one(values).getValue(0, 0);
+                will(returnValue(7.5));
+                one(values).getValue(0, 1);
+                will(returnValue(2.5));
+                one(values).getValue(0, 2);
+                will(returnValue(null));
+                one(values).getValue(0, 3);
+                will(returnValue(null));
+                one(values).getValue(0, 4);
+                will(returnValue(5));
+                one(values).getValue(0, 5);
+                will(returnValue(6));
+            }
+        });
+        int[] validcolumns = {0,2,3,5};
+        double result = DataUtilities.calculateRowTotal(values, 0, validcolumns);
+        assertEquals(13.5, result, .000000001d);
+    }
+
+    @Test // new
+    public void calculateRowTotalWithInValidColumns() {
+        Mockery mockingContext = new Mockery();
+        final Values2D values = mockingContext.mock(Values2D.class);
+        mockingContext.checking(new Expectations() {
+            {
+                one(values).getColumnCount();
+                will(returnValue(6));
+                one(values).getValue(0, 0);
+                will(returnValue(7.5));
+                one(values).getValue(0, 1);
+                will(returnValue(2.5));
+                one(values).getValue(0, 2);
+                will(returnValue(-3));
+                one(values).getValue(0, 3);
+                will(returnValue(-2));
+                one(values).getValue(0, 4);
+                will(returnValue(5));
+                one(values).getValue(0, 5);
+                will(returnValue(6));
+            }
+        });
+        int[] validcolumns = {10,22,13,15};
+        double result = DataUtilities.calculateRowTotal(values, 0, validcolumns);
+        assertEquals(0, result, .000000001d);
+
+    }
+
+
     // ---------------------------------------------------
     @Test
     public void createNumberArrayTest() {
@@ -157,6 +400,14 @@ public class DataUtilitiesTest extends DataUtilities {
 
     }
 
+    @Test // new
+    public void testCreateNumberArray() {
+        double[] input = { 1, 2, 3, 4, 5 };
+        Number[] expectedOutput = { 1.0, 2.0, 3.0, 4.0, 5.0 };
+        Number[] actualOutput = DataUtilities.createNumberArray(input);
+        assertArrayEquals(expectedOutput, actualOutput);
+    }
+
 
     //--------------------------------------------------------
     @Test // old
@@ -171,6 +422,14 @@ public class DataUtilitiesTest extends DataUtilities {
     public void testCreateNumberArray2DWithInvalidData() throws InvalidParameterException {
         double[][] input = null;
         DataUtilities.createNumberArray2D(input);
+    }
+
+    @Test // new
+    public void testCreateNumberArray2D() {
+        double[][] input = { { 1, 2 }, { 3, 4 }, { 5, 9 } };
+        Number[][] expectedOutput = { { 1.0, 2.0 }, { 3.0, 4.0 }, { 5.0, 9.0 } };
+        Number[][] actualOutput = DataUtilities.createNumberArray2D(input);
+        assertArrayEquals(expectedOutput, actualOutput);
     }
 
 
@@ -203,6 +462,148 @@ public class DataUtilitiesTest extends DataUtilities {
         KeyedValues result = DataUtilities.getCumulativePercentages(data);
         assertEquals("Cumulative percentages failed and the new KeyValues object contains wrong values.", expectedResult, result);
         context.assertIsSatisfied();
+    }
+
+    @Test // new
+    public void testGetCumulativePercentage() {
+        Mockery mockingContext = new Mockery();
+        final KeyedValues values = mockingContext.mock(KeyedValues.class);
+        mockingContext.checking(new Expectations() {
+            {
+                allowing(values).getItemCount();
+                will(returnValue(3));
+                allowing(values).getKeys();
+                will(returnIterator(0, 1, 2));
+                allowing(values).getValue(0);
+                will(returnValue(5));
+                allowing(values).getValue(1);
+                will(returnValue(9));
+                allowing(values).getValue(2);
+                will(returnValue(2));
+                allowing(values).getKey(0);
+                will(returnValue(0));
+                allowing(values).getKey(1);
+                will(returnValue(1));
+                allowing(values).getKey(2);
+                will(returnValue(2));
+            }
+        });
+        Mockery mocking = new Mockery();
+        final KeyedValues expectedOutput = mocking.mock(KeyedValues.class);
+        mocking.checking(new Expectations() {
+            {
+                one(expectedOutput).getItemCount();
+                will(returnValue(3));
+                one(expectedOutput).getKeys();
+                will(returnIterator(0, 1, 2));
+                one(expectedOutput).getValue(0);
+                will(returnValue(0.3125));
+                one(expectedOutput).getValue(1);
+                will(returnValue(0.875));
+                one(expectedOutput).getValue(2);
+                will(returnValue(1.0));
+            }
+        });
+        KeyedValues actualOutput = DataUtilities.getCumulativePercentages(values);
+        assertEquals(expectedOutput.getValue(0), actualOutput.getValue(0));
+        assertEquals(expectedOutput.getValue(1), actualOutput.getValue(1));
+        assertEquals(expectedOutput.getValue(2), actualOutput.getValue(2));
+    }
+
+    @Test // new
+    public void testGetCumulativePercentageWithNullValues() {
+        Mockery mockingContext = new Mockery();
+        final KeyedValues values = mockingContext.mock(KeyedValues.class);
+        mockingContext.checking(new Expectations() {
+            {
+                allowing(values).getItemCount();
+                will(returnValue(3));
+                allowing(values).getKeys();
+                will(returnIterator(0, 1, 2));
+                allowing(values).getValue(0);
+                will(returnValue(2));
+                allowing(values).getValue(1);
+                will(returnValue(null));
+                allowing(values).getValue(2);
+                will(returnValue(2));
+                allowing(values).getKey(0);
+                will(returnValue(0));
+                allowing(values).getKey(1);
+                will(returnValue(1));
+                allowing(values).getKey(2);
+                will(returnValue(2));
+            }
+        });
+        Mockery mocking = new Mockery();
+        final KeyedValues expectedOutput = mocking.mock(KeyedValues.class);
+        mocking.checking(new Expectations() {
+            {
+                one(expectedOutput).getItemCount();will(returnValue(3));
+                one(expectedOutput).getKeys();will(returnIterator(0, 1, 2));
+                one(expectedOutput).getValue(0);will(returnValue(0.50));
+                one(expectedOutput).getValue(1);will(returnValue(0.50));
+                one(expectedOutput).getValue(2);will(returnValue(1.0));
+            }
+        });
+        KeyedValues actualOutput = DataUtilities.getCumulativePercentages(values);
+        assertEquals(expectedOutput.getValue(0), actualOutput.getValue(0));
+        assertEquals(expectedOutput.getValue(1), actualOutput.getValue(1));
+        assertEquals(expectedOutput.getValue(2), actualOutput.getValue(2));
+    }
+
+    // Non categorized tests---------
+
+
+    @Test // new
+    public void testEqualWithAnEqualArrays() {
+        double[][] first = {{2,2}, {5,4}, {-9, 10}};
+        double[][] second = {{2,2}, {5,4}, {-9, 10}};
+        boolean expected = true;
+        boolean actual = DataUtilities.equal(first, second);
+        assertEquals(expected, actual);
+    }
+
+    @Test // new
+    public void testEqualWithAnNullArrays() {
+        double[][] firstArray = null;
+        double[][] secondArray = null;
+        boolean expected = true;
+        boolean actual = DataUtilities.equal(firstArray, secondArray);
+        assertEquals(expected, actual);
+    }
+
+    @Test // new
+    public void testEqualWithOneANullAndNonNull() {
+        double[][] first = {{1,4}, {4,4}, {-2, 13}};
+        double[][] second = null;
+        boolean expected = false;
+        boolean actual = DataUtilities.equal(first, second);
+        assertEquals(expected, actual);
+    }
+
+    @Test // new
+    public void testEqualWithADifferentLengthArrays() {
+        double[][] first = {{1,2}, {3,4}, {-9, 10}};
+        double[][] second = {{2,4}};
+        boolean expected = false;
+        boolean actual = DataUtilities.equal(first, second);
+        assertEquals(expected, actual);
+    }
+
+    @Test // new
+    public void testEqualWithSameLengthForDifferentArrays() {
+        double[][] first = {{4,2}, {3,4}, {-8, 10}};
+        double[][] second = {{4,2}, {3,5}, {-8, 10}};
+        boolean expected = false;
+        boolean actual = DataUtilities.equal(first, second);
+        assertEquals(expected, actual);
+    }
+
+    @Test // new
+    public void testCloneWithAValidArray() {
+        double[][] expected = {{1,2}, {3,4}, {-8, 11}};
+        double[][] actual = DataUtilities.clone(expected);
+        assertArrayEquals(expected, actual);
     }
 }
 
