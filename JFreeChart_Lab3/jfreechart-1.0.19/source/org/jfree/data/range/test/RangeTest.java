aa979013e95c0cc9e35168582d5b220eae0e22f3
Saman Pordanesh <sina.pordanesh@yahoo.com>
2023-03-03 18:01:18 -0700

    Range update

--- JFreeChart_Lab3/jfreechart-1.0.19/source/org/jfree/data/range/test/RangeTest.java
+++ JFreeChart_Lab3/jfreechart-1.0.19/source/org/jfree/data/range/test/RangeTest.java
@@ -129,6 +129,42 @@ public class RangeTest  {
         assertTrue("Expected output is True as 0.1 falls within the defined range.", exampleRange.contains(0.1));
     }
 
+    @Test // new
+    public void containsShouldBeFalseLower() {
+        assertFalse("The answer should be false",
+                exampleRange.contains(-5));
+    }
+
+    @Test // new
+    public void containsShouldBeFalseUpper() {
+        assertFalse("The answer should be false",
+                exampleRange.contains(6.3));
+    }
+
+    @Test // new
+    public void containsShouldBeTrueLowerBound() {
+        assertTrue("The answer should be true",
+                exampleRange.contains(-2));
+    }
+
+    @Test // new
+    public void containsShouldBeTrueUpperBound() {
+        assertTrue("The answer should be true",
+                exampleRange.contains(3));
+    }
+
+    @Test // new
+    public void containsShouldBeTrueBetween() {
+        assertTrue("The answer should be true",
+                exampleRange.contains(0));
+    }
+
+    @Test // new
+    public void containsCorrect() {
+        assertTrue("The answer should be true",
+                exampleRange.contains(1));
+    }
+
 
     // equals()---------------------------------
 
@@ -462,4 +498,225 @@ public class RangeTest  {
         String rangeString = "Range[-2.0,3.0]";
         assertEquals("Range using toString function should be Range[-2.0,3.0]", rangeString, exampleRange.toString());
     }
+
+    @Test // new
+    public void toStringNegativePositive() {
+        assertEquals("The answer should be 'Range[-2.0,3.0]'",
+                "Range[-2.0,3.0]" , exampleRange.toString());
+    }
+
+    @Test // new
+    public void toStringPositivePositive() {
+        Range R = new Range(2, 4.2);
+        assertEquals("The answer should be 'Range[2.0,4.2]'",
+                "Range[2.0,4.2]" , R.toString());
+    }
+
+    @Test // new
+    public void toStringNegativeNegative() {
+        Range R = new Range(-4.3, -1.2);
+        assertEquals("The answer should be 'Range[-4.3,-1.2]'",
+                "Range[-4.3,-1.2]" , R.toString());
+    }
+
+    // Test private
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+    //--------------------------- getUpperBound()
+    @Test
+    public void getUpperBoundShouldBeThree() {
+        assertEquals("The upper bound value of -2 and 3 should be 3",
+                3, exampleRange.getUpperBound(), .000000001d);
+    }
+
+    //--------------------------- getLength()
+    @Test
+    public void getLengthShouldBeFive() {
+        assertEquals("The central value of -2 and 3 should be 5",
+                5, exampleRange.getLength(), .000000001d);
+    }
+
+    //--------------------------- getCentralValue()
+    @Test
+    public void getCentralValueShouldBeZeroPointFive() {
+        assertEquals("The central value of -2 and 3 should be 0.5",
+                0.5, exampleRange.getCentralValue(), .000000001d);
+    }
+
+    //--------------------------- contains(double value)
+
+    //--------------------------- intersects(double b0, double b1)
+    @Test
+    public void intersectsShouldBeTrueLower() {
+        assertTrue("The lines interesect",
+                exampleRange.intersects(-3, 0));
+    }
+
+    @Test
+    public void intersectsShouldBeTrueMiddle() {
+        assertTrue("The lines interesect",
+                exampleRange.intersects(1, 2));
+    }
+
+    @Test
+    public void intersectsShouldBeTrueUpper() { // This test case is not working
+        assertTrue("The lines interesect",
+                exampleRange.intersects(2, 6));
+    }
+
+    @Test
+    public void intersectsShouldBeTrueLowerUpper() {
+        assertTrue("The lines interesect",
+                exampleRange.intersects(-4, 4));
+    }
+
+    @Test
+    public void intersectsShouldBeFalseLower() { // This test case is not working
+        assertFalse("The lines do not interesect",
+                exampleRange.intersects(-7, -4));
+    }
+
+    @Test
+    public void intersectsShouldBeFalseUpper() {
+        assertFalse("The lines do not interesect",
+                exampleRange.intersects(4, 6));
+    }
+
+    //--------------------------- intersects(Range range)
+    @Test
+    public void intersectsBoolean() {
+        Range R = new Range(0,1);
+        assertTrue("The lines interesect",
+                exampleRange.intersects(R));
+    }
+
+    @Test
+    public void intersectsBoolean2() {
+        Range R = new Range(0,1);
+        assertTrue("The lines interesect",
+                exampleRange.intersects(R));
+    }
+
+    //--------------------------- constrain()
+
+
+    //--------------------------- combine(Range range1, Range range2)
+    @Test
+    public void combineRange1Null() {
+        assertEquals("The range is -3 to 2",
+                exampleRange, Range.combine(null, exampleRange));
+    }
+
+    @Test
+    public void combineRange2Null() {
+        assertEquals("The range is -3 to 2",
+                exampleRange, Range.combine(exampleRange, null));
+    }
+
+    @Test
+    public void combineRight() {
+        Range R = new Range(1, 3);
+        Range C = new Range(-2, 3);
+        assertEquals("The range is -3 to 3",
+                C, Range.combine(exampleRange, R));
+    }
+
+
+
+    //--------------------------- expandToInclude(Range range, double value)
+
+
+    //--------------------------- expand(Range range, double lowerMargin, double upperMargin)
+    @Test
+    public void expandRange() {
+        Range R = new Range(18,23);
+        assertEquals("The range should be 18 to 23",
+                R, Range.expand(exampleRange, -4, 4));
+    }
+
+    //--------------------------- shift(Range base, double delta)
+    @Test
+    public void shiftTwoArgs() {
+        Range R = new Range(0, 5);
+        assertEquals("The range should be -1 to 5",
+                R, Range.shift(exampleRange, 2));
+    }
+
+    //--------------------------- shift(Range base, double delta, boolean allowZeroCrossing)
+    @Test
+    public void shiftTrue() {
+        Range R = new Range(0, 5);
+        assertEquals("The range should be 0 to 5",
+                R, Range.shift(exampleRange, 2, true));
+    }
+
+    @Test
+    public void shiftFalse() {
+        Range R = new Range(0, 5);
+        assertEquals("The range should be 0 to 5",
+                R, Range.shift(exampleRange, 2, false));
+    }
+
+    //--------------------------- scale(Range base, double factor)
+    @Test
+    public void scalePositive() {
+        Range R = new Range(-4,6);
+        assertEquals("The range should be -4 to 6",
+                R, Range.scale(exampleRange, 2));
+    }
+
+    @Test
+    public void scaleNegative() {
+        try {
+            assertEquals("The factor is negative",
+                    null, Range.scale(exampleRange, -2));
+        } catch(Exception e) {
+            throw e;
+        }
+    }
+
+    //--------------------------- equals(Object obj)
+
+
+    @Test
+    public void equalsIsNotRange() {
+        assertFalse("The Object is not a Range",
+                exampleRange.equals("Fluminense"));
+    }
+
+
+    @Test
+    public void equalsTrue() {
+        Range R = new Range(-2,3);
+        assertTrue("Both Ranges of exampleRange and R are equal",
+                exampleRange.equals(R));
+    }
+
+    //--------------------------- isNaNRange()
+    @Test
+    public void isNaNRangeTrue() {
+        assertFalse("The range is NaN",
+                exampleRange.isNaNRange());
+    }
+
+    //--------------------------- hashCode()
+    @Test
+    public void hashCodeTest() {
+        assertEquals("The hash code is 524288",
+                524288 , exampleRange.hashCode());
+    }
+
+    //--------------------------- toString()
+    */
 }
